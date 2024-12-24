2a17d62337f73adcccf1796897fd001772387a4d
silverwing2024 <baji4742@gmail.com>
2024-12-24 19:22:43 +0900

    feat:chapter01 생성

---
+++ baejihun_springboot/chapter00/src/main/java/me/baejihun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,29 @@
+package me.baejihun.springbootdeveloper;
+/*
+모든 프로젝트에는 main에 해당하는 클래스가 존재합니다 -> 실행용 클래스
+이제 이 class를 main 클래스로 사용할 예정입니다.
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class,args);
+    }
+}
+/*
+    처음으로 SpringBootDeveloperApplication 파일을 실행시키면 WhiteLabel error 페이지가 뜹니다.
+    현재 요청에 해당하는 페이지가 존재하지 않기 때문에 발생하는 문제입니다.
+    -> 하지만 스프링 애플리케이션은 실행됨.
+
+    그래서 error 페이지가 기분 나쁘니까 기본적으로 실행될 때의 default 페이지를 하나 생성하겠습니다.
+
+    20241223 MON.
+        1. 인텔리제이 커뮤니티 버전 설치 -> bin.path
+        2. Git 설치
+        3. GitHub 연동 -> Web_development_springboot -> 문제가 있음
+        4. 인텔리제이에 그래들 및 스프링 부트 프로젝트를 생성
+        5. POSTMAN을 설치
+*/
